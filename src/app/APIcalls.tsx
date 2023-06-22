bbf35192f6461a00ae6f9b80b2209bc35dcd11bb
Clément Le-Bas <clement.le-bas@ekino.com>
2023-06-22 10:36:26 +0200

    Added getScenario test with openai

--- src/app/APIcalls.tsx
+++ src/app/APIcalls.tsx
@@ -1,11 +1,44 @@
 export const fetchCharacters = async () => {
-    try {
-      const response = await fetch(
-        `https://gateway.marvel.com/v1/public/characters?apikey=${process.env.NEXT_PUBLIC_API_KEY_MARVEL}&orderBy=-modified&limit=100`
-      );
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log('Error fetching characters:', error);
-    }
-  };
+  try {
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY_MARVEL;
+    const apiUrl = 'https://gateway.marvel.com/v1/public/characters';
+
+    const response = await fetch(`${apiUrl}?apikey=${apiKey}&orderBy=-modified&limit=100`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Error fetching characters:', error);
+  }
+};
+
+export const getScenario = async (charactersOne?: String, characterTwo?: String) => {
+  try {
+    const apiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
+    const apiUrl = 'https://api.openai.com/v1/completions';
+
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: `Act as Stan Lee.
+          Give me a story with conversation between ${charactersOne} and ${characterTwo} based on the characteristics of each super hero.
+          The narrator should speack at least 3 times.
+          Write the name of the speaker before each chat. If it's the narrator, write "Narrator:"
+          `,
+        max_tokens: 1000,
+        temperature: 0,
+      }),
+    };
+
+    const response = await fetch(apiUrl, requestOptions);
+    const data = await response.json();
+
+    return data.choices[0].text;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
--- src/pages/secondPage.tsx
+++ src/pages/secondPage.tsx
@@ -1,5 +1,19 @@
 'use client';
 
+import { useEffect } from 'react';
+
+import { getScenario } from '../app/APIcalls';
+
 export default function SecondPage() {
-  return <div>Test</div>
+  function generatePrompt(charactersOne?: String, characterTwo?: String) {
+    return 'Test';
+  }
+
+  useEffect(() => {
+    getScenario('Spider-man', 'Iron-man').then((response) => {
+      console.log('response', response);
+    });
+  }, []);
+
+  return <div>{}</div>;
 }
