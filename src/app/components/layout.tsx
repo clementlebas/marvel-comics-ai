52776b18aa206e6bdf91aa320c85c2d6fa77f801
Clément Le-Bas <clement.le-bas@ekino.com>
2023-06-23 16:30:38 +0200

    Added redux Provider to _app

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,10 +1,8 @@
 import { Inter } from 'next/font/google';
-import { Provider } from 'react-redux';
 
-import { store } from '../redux/store';
 import styles from '../styles/page.module.css';
 import '../styles/globals.css';
-import "nprogress/nprogress.css";
+import 'nprogress/nprogress.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <div className={`${styles.description}`}>
         <p>Marvel Project by Clément Le Bas</p>
       </div>
-      <Provider store={store}>{children}</Provider>
+      {children}
     </main>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+
+import { store } from '../app/redux/store';
 import Layout from '../app/components/layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
